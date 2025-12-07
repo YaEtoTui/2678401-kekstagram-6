ca5afc115b8baaae2fd455b61a33f14123b872fe
Egor Sazhin <e.sazhin@goodt.me>
2025-12-07 23:59:28 +0500

    module9-task1 - Правда или действие

--- js/util.js
+++ js/util.js
@@ -38,4 +38,20 @@ const createRandomIdFromRangeGenerator = function (min, max) {
   };
 };
 
+const showModal = (currentElement, onEscape) => {
+  currentElement.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onEscape);
+}
+
+const hideModal = (currentElement, onEscape) => {
+  currentElement.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onEscape);
+}
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {showModal, hideModal, isEscapeKey };
+
 export const generateCommentId = createRandomIdFromRangeGenerator(1, countComments);
