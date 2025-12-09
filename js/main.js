a0b344d774cf9768d145a8a70972fc301569a19d
Egor Sazhin <e.sazhin@goodt.me>
2025-12-09 14:59:56 +0500

    module12-task1 - Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -5,10 +5,16 @@ import {showImageModal} from './big-picture.js';
 import './form.js';
 import './scale.js';
 import './effects.js';
+import {imgFilters} from './filters.js';
+import {sortImgList} from './img-sort.js';
 
 const onPhotoClick = (photosData) => {
   showImageModal(photosData);
 };
 
 getData()
-  .then((pictures) => renderPictures(pictures, onPhotoClick));
+  .then((pictures) => {
+    renderPictures(pictures, onPhotoClick);
+    sortImgList(pictures, onPhotoClick);
+    imgFilters.classList.remove('img-filters--inactive');
+  });
